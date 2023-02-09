1dcd019f6fc357391bea47fa630b926ef6e3096b
Gek <gakidogeck@gmail.com>
2023-02-08 19:49:46 -0800

    Add User logout backend endpoint

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -29,5 +29,13 @@ router.post(
     }
   );
 
+  // Log out route will remove the token cookie from the response and return a JSON success message.
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
 
 module.exports = router;
