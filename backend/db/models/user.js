0966f26f3604f95f1c75dc55018312b8d3dbe0dd
Gek <gakidogeck@gmail.com>
2023-02-14 20:18:34 -0800

    added hasMany to models

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -58,6 +58,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Spot, { foreignKey: 'ownerId' })
+      User.hasMany(models.Booking, { foreignKey: 'userId'})
     }
   }
   User.init({
