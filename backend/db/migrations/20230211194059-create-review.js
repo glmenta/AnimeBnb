587ccaf3b80bf37886959a01bc794a66c9e937fb
Gek <gakidogeck@gmail.com>
2023-02-15 20:47:11 -0800

    added options to migrations

--- backend/db/migrations/20230211194059-create-review.js
+++ backend/db/migrations/20230211194059-create-review.js
@@ -42,7 +42,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Reviews');
