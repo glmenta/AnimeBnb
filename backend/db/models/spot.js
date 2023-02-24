10b57bd34e9b55068b7a80b1a176f279c5c85479
Gek <gakidogeck@gmail.com>
2023-02-23 22:27:11 -0800

    added seq.literal to spot model

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -76,31 +76,13 @@ module.exports = (sequelize, DataTypes) => {
     scopes: {
       queryFilter() {
         return {
-          attributes: [
-            'id',
-            'ownerId',
-            'address',
-            'city',
-            'state',
-            'country',
-            'lat',
-            'lng',
-            'name',
-            'description',
-            'price',
-            'createdAt',
-            'updatedAt',
-            [
-              Sequelize.literal(
-                `(SELECT ROUND(AVG(stars), 1) FROM ${schema ? `"${schema}"."Reviews"` : 'Reviews'
-                } WHERE "Reviews"."spotId" = "Spot"."id")`
+          attributes: [ 'id', 'ownerId', 'address', 'city', 'state', 'country','lat','lng','name','description','price','createdAt','updatedAt',
+            [ Sequelize.literal(`(SELECT ROUND(AVG(stars), 1) FROM ${schema ? `"${schema}"."Reviews"` : 'Reviews'} WHERE "Reviews"."spotId" = "Spot"."id")`
               ),
               'avgRating',
             ],
             [
-              Sequelize.literal(
-                `(SELECT url FROM ${schema ? `"${schema}"."SpotImages"` : 'SpotImages'
-                } WHERE "SpotImages"."spotId" = "Spot"."id" AND "SpotImages"."preview" = true LIMIT 1)`
+              Sequelize.literal(`(SELECT url FROM ${schema ? `"${schema}"."SpotImages"` : 'SpotImages'} WHERE "SpotImages"."spotId" = "Spot"."id" AND "SpotImages"."preview" = true LIMIT 1)`
               ),
               'previewImage',
             ],
