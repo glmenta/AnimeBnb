40f5711f693d74db2d47c96a820fc6e5705febcd
Gek <gakidogeck@gmail.com>
2023-02-25 14:20:47 -0800

    float to decimal, 2 to 1

--- backend/db/migrations/20230211193938-create-spot.js
+++ backend/db/migrations/20230211193938-create-spot.js
@@ -39,10 +39,10 @@ module.exports = {
         allowNull: false
       },
       lat: {
-        type: Sequelize.FLOAT,
+        type: Sequelize.DECIMAL,
       },
       lng: {
-        type: Sequelize.FLOAT,
+        type: Sequelize.DECIMAL,
       },
       name: {
         type: Sequelize.STRING,
