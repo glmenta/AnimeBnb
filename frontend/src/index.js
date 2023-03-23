bda1d4cb5698e888bb764fd3cc233ebdd61bf8a6
Gek <gakidogeck@gmail.com>
2023-03-22 21:42:13 -0700

    modal phase 4

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { restoreCSRF, csrfFetch } from './store/csrf';
-import * as sessionActions from './store/session';
+import { ModalProvider, Modal } from './context/Modal';
+import App from './App';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
 }
 
+// Wrap the application with the Modal provider and render the Modal component
+// after the App component so that all the Modal content will be layered as
+// HTML elements on top of the all the other HTML elements:
 function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <div className = 'body-bg'>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
+    </div>
   );
 }
 
