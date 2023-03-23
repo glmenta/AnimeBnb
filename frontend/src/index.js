93593d831ff407603b438cced101dc6176ddb67d
Gek <gakidogeck@gmail.com>
2023-03-22 20:20:50 -0700

    loginform step done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,16 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-
+import * as sessionActions from './store/session';
 import configureStore from './store';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
   restoreCSRF();
-
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
@@ -29,7 +29,7 @@ function Root() {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
