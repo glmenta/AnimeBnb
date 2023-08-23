c3ad946aa5cc6b6c3a3f94be9e7efc111c00fd3a
Gek <gakidogeck@gmail.com>
2023-08-23 02:05:51 -0700

    added footer

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <a href="https://github.com/glmenta" target="_blank" rel="noopener noreferrer">GitHub</a>
+            |
+            <a href="https://www.linkedin.com/in/geryko-menta-36a0b0215/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </div>
+    );
+}
+
+export default Footer;
